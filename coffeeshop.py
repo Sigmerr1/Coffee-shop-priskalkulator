fd3e785186cbb73a7be88b74432ffed806769d41
Sigmerr1 <08leawal@elev.danielsen-skoler.no>
2025-09-11 09:29:57 +0200

    Har lagt til alle drikkene, har lagt til størrelse og take away eller ikke.

--- coffeeshop.py
+++ coffeeshop.py
@@ -1,8 +1,7 @@
-#Kilde: The Coffee Shop Price Calculator - www.101computing.net/the-coffee-shop-price-calculator
 print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
 print("+                               +")
-print("+         The Coffee Shop       +")
-print("+              Welcome          +")
+print("+     Leander´s Coffee Shop     +")
+print("+           Welcome             +")
 print("+                               +")
 print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
 print("")
@@ -17,14 +16,39 @@ print(" > Flat White")
 print("----------------------------")
 
 price = 0
-coffee = input("What type of coffee would you like?").title()
+coffee = input("What coffee do you want? ").title()
 if coffee=="Espresso":
    price = price + 2.50
 elif coffee=="Americano":
    price = price + 3
 elif coffee=="Latte":
    price = price + 2.50
+elif coffee=="Cappuccino":
+   price = price + 3
+elif coffee=="Macchiato":
+   price = price + 2.50
+elif coffee=="Mocha":
+   price = price + 3.50
+elif coffee=="Flat White":
+   price = price + 2.50
+else:
+   print("We dont have that Coffee.")
 
-#Complete the code here...
+size = input("What size you want? (Medium, Large, XL) ").title()
+if size=="Medium":
+   price = price + 0
+elif size=="Large":
+   price = price + 1.00
+elif size=="XL":
+   price = price + 1.50
+else:
+   print("We only have medium, large and XL.")
+
+takeaway = input("Eat in or take away? ").title()
+if takeaway=="Eat in":
+   price = price + 0
+elif takeaway=="Take away":
+   price = price + 1.00
+
 print("----------------------------")
 print("Total Cost: £" + str(price))

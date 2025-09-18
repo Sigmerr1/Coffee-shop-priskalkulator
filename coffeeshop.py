b4b238a94a358262bedeba1bb10f612ed4040eb7
Sigmerr1 <08leawal@elev.danielsen-skoler.no>
2025-09-18 08:53:37 +0200

    la till komentarer som forklarer koden

--- coffeeshop.py
+++ coffeeshop.py
@@ -5,7 +5,7 @@ print("+           Welcome             +")
 print("+                               +")
 print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
 print("")
-print("We serve the following coffees, which:")
+print("We serve the following coffees, which:") #alle de forskjelllige valgene
 print(" > Espresso")
 print(" > Americano")
 print(" > Latte")
@@ -15,15 +15,18 @@ print(" > Mocha")
 print(" > Flat White")
 print("----------------------------")
 
+#start pris
 price = 0
 
+#en loop som spør hva du vil ha helt til du gir et riktig svar
 while True:
    coffee = input("What coffee do you want? ").title()
    if coffee in ["Espresso", "Americano", "Latte", "Cappuccino", "Macchiato", "Mocha", "Flat White"]:
       break
    else:
       print("We dont have that Coffee. Please choose from the menu.")
-      
+
+#Prisen på de forskjellige kaffene
 if coffee=="Espresso":
    price = price + 2.50
 elif coffee=="Americano":
@@ -41,6 +44,7 @@ elif coffee=="Flat White":
 else:
    print("We dont have that Coffee.")
 
+#spør hvilken størrelse du vil ha helt til du svarer riktig
 while True:
    size = input("What size do you want? (Medium, Large, Xl) ").title()
    if size in ["Medium", "Large", "Xl"]:
@@ -57,6 +61,7 @@ elif size=="Xl":
 else:
    print("We only have medium, large and XL.")
 
+#spør om du vil sitte inne eller ute helt til du svare riktig
 while True:
    takeaway = input("Eat In or Take Away? ").title()
    if takeaway in ["Eat In", "Take Away"]:
@@ -64,13 +69,16 @@ while True:
    else:
       print("We cant provide that. Please choose to eat in or take away.")
 
+#pris for take away og eat in
 if takeaway=="Eat In":
    price = price + 0
 elif takeaway=="Take Away":
    price = price + 1.00
 
+#regner sammen prisene og sier hvor mye det koster
 print("----------------------------")
 print("Total Cost: £" + str(price))
 
+#en hygglig melding som sier hva du har bestilt
 print("Thanks for visiting Leander´s Coffee Shop. Enjoy your " + size + " " + coffee + " :D")
 
